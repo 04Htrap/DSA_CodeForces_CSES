548a3c00976af80ba61465e6e6795b95aa02884f
04Htrap <go.parth04@gmail.com>
2025-08-02 20:43:32 +0530

    02/08

--- CodeForces/800-900/D_Stay_or_Mirror.cpp
+++ CodeForces/800-900/D_Stay_or_Mirror.cpp
@@ -9,19 +9,33 @@
 #define ll long long
 using namespace std;
 
-void functionName(){
-    
+void functionName() {
+    int n, ans = 0;
+    cin >> n;
+    vector<int> p(n);
+    for(int i = 0; i < n; i++) cin >> p[i];
+
+    for(int i = 0; i < n; i++) {
+        int left = 0, right = 0;
+        for(int j = 0; j < i; j++) 
+            if(p[j] > p[i]) left++;
+        for(int j = i + 1; j < n; j++) 
+            if(p[j] > p[i]) right++;
+        ans += min(left, right);
+    }
+
+    cout << ans << '\n';
 }
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
         
     int t;
     cin >> t;
 
-    while(t--){
-        
+    while(t--) {
+        functionName();
     }
 
     return 0;
